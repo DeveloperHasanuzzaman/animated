5e6a733b89e21764a9004f167ac56f83bdf48a72
DeveloperHasanuzzaman <70207075+DeveloperHasanuzzaman@users.noreply.github.com>
2020-09-20 04:24:24 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+    $('button').click(function(){
+
+
+        var m = $('div');
+        m.animate({
+            left:'300px',
+            width:'500px',
+            height:'500px',
+            opacity:'0.5'
+        
+            
+        })
+    });
+});
+
+
+
